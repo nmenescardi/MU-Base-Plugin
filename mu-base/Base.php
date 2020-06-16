f7bd637b59fefe3e953f15cf1ff0a5f224b088fb
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-16 09:42:52 -0300

    Add basic init methods

--- mu-base/Base.php
+++ mu-base/Base.php
@@ -10,6 +10,30 @@ class Base
 {
 
   use Pluggable;
+
+  public function __construct()
+  {
+    $this->constants();
+    $this->filters();
+    $this->actions();
+  }
+
+  public function constants()
+  {
+    if (!defined('MUBASE_PLUGIN_PATH'))
+      define('MUBASE_PLUGIN_PATH', trailingslashit(plugin_dir_path(__FILE__)));
+
+    if (!defined('MUBASE_PLUGIN_URL'))
+      define('MUBASE_PLUGIN_URL', trailingslashit(plugin_dir_path(__FILE__)));
+  }
+
+  public function filters()
+  {
+  }
+
+  public function actions()
+  {
+  }
 }
 
 
