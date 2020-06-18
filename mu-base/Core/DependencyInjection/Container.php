4ab1df7ce660165aed8c595cf4cdecc8fbe578cf
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-18 09:09:55 -0300

    Register Services

--- mu-base/Core/DependencyInjection/Container.php
+++ mu-base/Core/DependencyInjection/Container.php
@@ -2,12 +2,20 @@
 
 namespace MUBase\Core\DependencyInjection;
 
+use Pimple\ServiceProviderInterface;
+
 class Container extends \Pimple\Container
 {
 
   public function registerServices(array $services)
   {
     foreach ($services as $service) {
+
+      $service = new $service();
+
+      if (!$service instanceof ServiceProviderInterface)
+        throw new \InvalidArgumentException('service object must implement the "ServiceProviderInterface".');
+
       $this->register($service);
     }
   }
