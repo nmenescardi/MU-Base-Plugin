55f5874c5ca69413bdf6627290fbfc2807f7a2da
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-28 12:08:40 -0300

    Add Related Scope as specific to only one CPT

--- mu-base/Core/Models/Posts/Example.php
+++ mu-base/Core/Models/Posts/Example.php
@@ -7,6 +7,10 @@ use MUBase\Core\PostTypes\CptExample;
 class Example extends AbstractPostRepository
 {
 
+  protected $concrete_scopes = [
+    'related'  => \MUBase\Core\Models\Scopes\Related::class,
+  ];
+
   public static function key(): string
   {
     return CptExample::key();
---
+++ mu-base/Core/Models/Scopes/Related.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MUBase\Core\Models\Scopes;
+
+class Related extends AbstractScope
+{
+
+  protected function filterParams()
+  {
+    $this->taxonomyKey =
+      (is_array($this->rawParams) && isset($this->rawParams[0]))
+      ? $this->rawParams[0]
+      : '';
+  }
+
+  protected function concreteArgs(): array
+  {
+    $postID = 5; //TODO: exclude itself
+
+    $args = ['post__not_in'  => array($postID)];
+
+    $terms = get_the_terms(
+      $postID,
+      $this->taxonomyKey
+    );
+
+    if (!empty($terms)) {
+
+      $termSlug = array_shift($terms);
+
+      $args['tax_query'] = [
+        [
+          'taxonomy'  => $this->taxonomyKey,
+          'field'     => 'slug',
+          'terms'     => $termSlug,
+        ]
+      ];
+    }
+
+    return $args;
+  }
+}
