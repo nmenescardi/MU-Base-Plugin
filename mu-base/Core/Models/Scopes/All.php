e502d55d92c105cba2c309e8a016313d7ac3a057
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-28 10:26:40 -0300

    No filterParam method needed on All Scope class

--- mu-base/Core/Models/Scopes/All.php
+++ mu-base/Core/Models/Scopes/All.php
@@ -5,11 +5,6 @@ namespace MUBase\Core\Models\Scopes;
 class All extends AbstractScope
 {
 
-  protected function filterParams()
-  {
-    //TODO: Check the first Param is an ID or array of IDs to filter
-  }
-
   public function getArgs(): array
   {
     return [
