fcaf590581150c622ca31778bcd0b56468d8dbd8
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-06-28 11:16:07 -0300

    Add Dynamic Param to Latest Scope

--- mu-base/Core/Models/Scopes/Latest.php
+++ mu-base/Core/Models/Scopes/Latest.php
@@ -5,10 +5,19 @@ namespace MUBase\Core\Models\Scopes;
 class Latest extends AbstractScope
 {
 
+
+  protected function filterParams()
+  {
+    $this->postsPerPage =
+      (is_array($this->rawParams) && isset($this->rawParams[0]))
+      ? $this->rawParams[0]
+      : false;
+  }
+
   protected function concreteArgs(): array
   {
     return [
-      'posts_per_page'  => 15,
+      'posts_per_page'  => $this->postsPerPage ?: 15,
       'orderby'         => 'date',
       'order'           => 'DESC',
     ];
