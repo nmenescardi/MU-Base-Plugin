41035e3e6353cd8a205989b6a08af3daf59dcec3
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-07-04 13:16:03 -0300

    convert multiple authors args as array to comma separated string

--- mu-base/Core/Models/Scopes/ByAuthor.php
+++ mu-base/Core/Models/Scopes/ByAuthor.php
@@ -11,6 +11,10 @@ class ByAuthor extends AbstractScope
       (is_array($this->rawParams) && isset($this->rawParams[0]))
       ? $this->rawParams[0]
       : false;
+
+    // If array -> convert to comma separated string
+    if (is_array($this->filteredAuthorIDs))
+      $this->filteredAuthorIDs = implode(',', $this->filteredAuthorIDs);
   }
 
   protected function concreteArgs(): array
