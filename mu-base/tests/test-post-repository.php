961f2b65bbcc155ce5e55af8f842101fbe2afca6
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-07-04 12:55:20 -0300

    add get_latest_scope_with_pagination

--- mu-base/tests/test-post-repository.php
+++ mu-base/tests/test-post-repository.php
@@ -25,4 +25,21 @@ class PostRepositoryTest extends WP_UnitTestCase
 
 		$this->assertCount(25, $all);
 	}
+
+	/** @test */
+	public function get_latest_scope_with_pagination()
+	{
+		$example_model = new \MUBase\Core\Models\Posts\Example();
+
+		$posts = $this->factory->post->create_many(
+			25,
+			[
+				'post_type' => 'cpt-example'
+			]
+		);
+
+		$latest = $example_model->latest(20);
+
+		$this->assertCount(20, $latest);
+	}
 }
