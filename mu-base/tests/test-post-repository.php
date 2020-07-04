264cad31444da234bef5b3e8aa6e2dbbeb16db37
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-07-04 12:52:19 -0300

    Improve All Scope test

--- mu-base/tests/test-post-repository.php
+++ mu-base/tests/test-post-repository.php
@@ -6,22 +6,23 @@ class PostRepositoryTest extends WP_UnitTestCase
 	public function setup()
 	{
 		parent::setUp();
-
-		$this->postID = $this->factory->post->create([
-			'post_type' => 'cpt-example'
-		]);
 	}
 
 	/** @test */
 	public function get_all_scope()
 	{
 
-		// Example Model
 		$example_model = new \MUBase\Core\Models\Posts\Example();
 
-		// All
+		$posts = $this->factory->post->create_many(
+			25,
+			[
+				'post_type' => 'cpt-example'
+			]
+		);
+
 		$all = $example_model->all();
 
-		$this->assertCount(1, $all);
+		$this->assertCount(25, $all);
 	}
 }
