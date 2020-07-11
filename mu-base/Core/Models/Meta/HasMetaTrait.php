0f0389c806e6d3a710dfce35b67ab1e71c657c8f
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2020-07-11 14:23:28 -0300

    add HasMetaTrait

---
+++ mu-base/Core/Models/Meta/HasMetaTrait.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace MUBase\Core\Models\Meta;
+
+trait HasMetaTrait
+{
+
+  // To be overriden by different models. 
+  // Should be 'post', 'comment', 'term', 'user', or any other object type with an associated meta table.
+  protected $meta_type = 'post';
+
+  /**
+   * Insert or Update metadata
+   *
+   * @param string $key
+   * @param [type] $value
+   * @return int|bool
+   */
+  public function saveMeta(string $key, $value)
+  {
+    return \update_metadata(
+      $this->meta_type,
+      $this->ID,
+      $key,
+      $value
+    );
+  }
+
+  public function getMeta(string $key, $default = false, bool $single = false)
+  {
+    $metaValue = \get_metadata(
+      $this->meta_type,
+      $this->ID,
+      $key,
+      $single
+    );
+
+    return $metaValue ?: $default;
+  }
+
+  public function deleteMeta(string $key, $value = '', bool $deleteAll = false)
+  {
+    return \delete_metadata(
+      $this->meta_type,
+      $this->ID,
+      $key,
+      $value,
+      $deleteAll
+    );
+  }
+}
